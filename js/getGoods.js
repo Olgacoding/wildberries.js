f93a8a948ea20287df6e0194f8af63ac6e52ca76
Olgacoding <92987743+Olgacoding@users.noreply.github.com>
2021-11-20 18:11:52 +0200

    Add files via upload

--- js/getGoods.js
+++ js/getGoods.js
@@ -3,3 +3,11 @@ const getGoods = function () {
     fetch('/db/db.json')
 }
 getGoods()
+
+// getGoods.js:3 Fetch API cannot load file:///C:/db/db.json. URL scheme "file" is not supported.
+// getGoods @ getGoods.js:3
+// 17:34:27.629 getGoods.js:3  Uncaught (in promise) TypeError: Failed to fetch
+    // at getGoods (getGoods.js:3)
+    // at getGoods.js:5 - такая у меня в консоле ошибка
+
+    // network > all> а name и headers, как у Вас на скриншоте нету(
