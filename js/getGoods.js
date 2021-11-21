407808df380c0e6c93c505b70c416847e763524f
Olgacoding <92987743+Olgacoding@users.noreply.github.com>
2021-11-21 19:25:00 +0200

    Add files via upload

--- js/getGoods.js
+++ js/getGoods.js
@@ -1,13 +1,102 @@
 const getGoods = function () {
+    const links = document.querySelectorAll('.navigation-link')
+
+    const renderGoods = (goods) => {
+        const goodsContainer = document.querySelector('.long-goods-list')
+
+        goodsContainer.innerHTML = ""
+
+        goods.forEach(good => {
+            const goodBlock = document.createElement('div')
+
+            goodBlock.classList.add('col-lg-3')
+            goodBlock.classList.add('col-sm-6')
+
+            goodBlock.innerHTML = `
+            <div class="goods-card">
+                <span class="label ${good.label ? null : 'd-none'}">${good.label}</span>
+                <img src="db/${good.img}" alt="${good.name}" class="goods-image">
+                <h3 class="goods-title">${good.name}</h3>
+				<p class="goods-description">${good.description}</p>
+                <button class="button goods-card-btn add-to-cart" data-id="${good.id}">
+					<span class="${good.price}">$89</span>
+				</button >
+            </div >
+    `
+            goodsContainer.append(goodBlock)
+
+            console.log(good);
+        })
+
+    }
+
+
+
     // console.log('getGoods');
-    fetch('/db/db.json')
+    const getData = (value, category) => {
+        fetch('/db/db.json')
+            // .then(function (response) {
+            //     // console.log(response);
+            //     return response.json()
+            // })
+            // .then(function (data) {
+            //     console.log(data)
+            // })
+
+            .then((res) => res.json())
+            // console.log(response);
+            .then((data) => {
+                // console.log(data)
+                // перебор массива filter
+                const array = category ? data.filter((item) => item[category] === value) : data
+
+                // if (category) {
+                //     console.log('есть')
+                // } else {
+                //     console.log('нет')
+                // }
+
+                // category ? console.log('есть') : console.log('нет')
+                // тернарный оператор
+
+
+
+                localStorage.setItem('goods', JSON.stringify(array))
+                // localStorage.setItem('goods', JSON.stringify(data))
+
+                if (window.location.pathname !== "/goods.html") {
+                    window.location.href = '/goods.html';
+                } else {
+                    renderGoods(array)
+                }
+                // window.location.href = '/goods.html';
+                // console.log(window.location);
+            })
+    }
+
+    links.forEach((link) => {
+        link.addEventListener('click', (event) => {
+            event.preventDefault()
+            const linkValue = link.textContent
+            const category = link.dataset.field
+
+            getData(linkValue, category)
+        })
+    })
+    if (localStorage.getItem('goods') && window.location.pathname === "/goods.html") {
+        renderGoods(JSON.parse(localStorage.getItem('goods')))
+    }
+
+    localStorage.setItem('goods', JSON.stringify({ name: 'all' }))
+    // сохранили в локалсторе обьект
+    localStorage.setItem('goods', JSON.stringify([1, 2, 3, 4, 5]))
+    // сохранили массив
+    // console.log(localStorage.getItem('goods'));
+    const goods = JSON.parse(localStorage.setItem('goods'));
+    console.log(goods)
+
+    localStorage.removeItem('goods')
+    console.log(localStorage)
 }
 getGoods()
 
-// getGoods.js:3 Fetch API cannot load file:///C:/db/db.json. URL scheme "file" is not supported.
-// getGoods @ getGoods.js:3
-// 17:34:27.629 getGoods.js:3  Uncaught (in promise) TypeError: Failed to fetch
-    // at getGoods (getGoods.js:3)
-    // at getGoods.js:5 - такая у меня в консоле ошибка
-
-    // network > all> а name и headers, как у Вас на скриншоте нету(
--- js/search.js
+++ js/search.js
@@ -3,6 +3,72 @@ const search = function () {
     const input = document.querySelector('.search-block > input')
     const searchBtn = document.querySelector('.search-block > button')
 
+
+    const renderGoods = (goods) => {
+        const goodsContainer = document.querySelector('.long-goods-list')
+
+        goodsContainer.innerHTML = ""
+
+        goods.forEach(good => {
+            const goodBlock = document.createElement('div')
+
+            goodBlock.classList.add('col-lg-3')
+            goodBlock.classList.add('col-sm-6')
+
+            goodBlock.innerHTML = `
+            <div class="goods-card">
+                <span class="label ${good.label ? null : 'd-none'}">${good.label}</span>
+                <img src="db/${good.img}" alt="${good.name}" class="goods-image">
+                <h3 class="goods-title">${good.name}</h3>
+				<p class="goods-description">${good.description}</p>
+                <button class="button goods-card-btn add-to-cart" data-id="${good.id}">
+					<span class="${good.price}">$89</span>
+				</button >
+            </div >
+    `
+            goodsContainer.append(goodBlock)
+
+            console.log(good);
+        })
+
+    }
+
+
+
+    // console.log('getGoods');
+    const getData = (value) => {
+        fetch('/db/db.json')
+            .then((res) => res.json())
+            .then((data) => {
+                // перебор массива filter
+                const array = data.filter(good => {
+                    return good.name.toLowerCase().includes(value.toLowerCase())
+                })
+                // if (category) {
+                //     console.log('есть')
+                // } else {
+                //     console.log('нет')
+                // }
+
+                // category ? console.log('есть') : console.log('нет')
+                // тернарный оператор
+                localStorage.setItem('goods', JSON.stringify(array))
+                // localStorage.setItem('goods', JSON.stringify(data))
+
+                if (window.location.pathname !== "/goods.html") {
+                    window.location.href = '/goods.html';
+                } else {
+                    renderGoods(array)
+                }
+                // window.location.href = '/goods.html';
+                // console.log(window.location);
+            })
+    }
+
+
+
+
+
     console.log(input);
     console.log(searchBtn);
 
@@ -13,11 +79,21 @@ const search = function () {
 
 
 
+    try {
+        searchBtn.addEventListener('click', () => {
+            // console.log(searchBtn);
+            console.log(input.value);
+        })
+    } catch (e) {
+        console.error('Уважаемый верстальщик, верните пожалуйста класс');
+    }
+
 
     searchBtn.addEventListener('click', () => {
         // console.log(searchBtn);
-        console.log(input.value);
-    }) 
+        console.log();
+        getData(input.value)
+    })
     // HOMEWORK  - check homework and correct me please.
 }
 
